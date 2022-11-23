c6132d28d439d490887ac696562940cddebff172
Yasmin-A95 <107013100+Yasmin-A95@users.noreply.github.com>
2022-11-23 18:48:30 +1100

    Made server side and client side changes - Server : I changed package json to reflect filename, added .env, and changed some mongodb connection stuff - Client : Commented out some unused variables to help fix the bug

--- server/server.js
+++ server/server.js
@@ -3,27 +3,27 @@ const app = express();
 const mongoose = require("mongoose");
 const cors = require("cors");
 const WhiteCoffeeModel = require("./models/Coffee")
+const PORT = process.env.PORT || 3001
 
-
+app.use(cors());
 app.use(express.json());
 
-mongoose.connect("mongodb+srv://charlesJprince:charlesjoma2@cluster0.nv1odnc.mongodb.net/coffee_orders?retryWrites=true&w=majority", 
+mongoose.connect("mongodb+srv://charlesJprince:charlesjoma2@cluster0.nv1odnc.mongodb.net/coffee_orders?retryWrites=true&w=majority",
     {
         useNewUrlParser: true
     }
 );
-
-app.get("/", async(req, res) => {
-    const coffee = new WhiteCoffeeModel({ coffeeName: "Flat White", coffeeMilk: "Oat", coffeeSize: "Large"})
-
-    try {
-        await coffee.save();
-    }catch(error){
-        console.log(error)
-    }
+const db = mongoose.connection
+db.on('error', err => {
+    console.log('error connecting to server', err)
+    process.exit(1)
 })
 
-app.listen(3001, () => {
-    console.log("Server running on Port 3001")
-});
+app.get("/", async (req, res) => {
+    const coffee = new WhiteCoffeeModel({ coffeeName: "Flat White", coffeeMilk: "Oat", coffeeSize: "Large" })
+    res.json(coffee)
+})
 
+app.listen(PORT, () => {
+    console.log(`Server running on ${PORT}`)
+});
